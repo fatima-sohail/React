d5c9acab2ee09ec66f78117132d69e2933324836
unknown <fatimasohailhunzai@gmail.com>
2023-10-21 18:51:08 -0400

    Added color-picker in react folder

---
+++ color-picker/src/ColorPicker.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+const colorNames = ['Aquamarine', 'BlueViolet', 'Chartreuse', 'CornflowerBlue', 'Thistle', 'SpringGreen', 'SaddleBrown', 'PapayaWhip', 'MistyRose'];
+
+ function ColorPicker() {
+  const [color, setColor] = useState(null);
+
+ const divStyle = {backgroundColor: color};
+
+  return (
+    <div style={divStyle}>
+      <p>Selected color: {color}</p>
+      {colorNames.map((colorName)=>(
+        <button 
+          onClick={() => setColor(colorName)} 
+          key={colorName}>
+       	     {colorName}
+      	</button>
+      ))}
+    </div>
+  );
+}
+
+
+export default ColorPicker;
