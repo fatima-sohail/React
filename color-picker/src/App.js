0656ecd154d15943720ea3a0486fd205e5a18ff7
unknown <fatimasohailhunzai@gmail.com>
2023-10-24 16:07:22 -0400

    changed color in app.css in  full-screen class selector

--- color-picker/src/App.js
+++ color-picker/src/App.js
@@ -3,6 +3,10 @@ import ColorPicker from './ColorPicker';
 
 function App() {
   return (
+    /*!!! work needs to be done- full-screen only renders the color to a small portion 
+    in the center of the laptop screen
+    need to work on getting the the entire screen filled with color
+    */
     <div className='full-screen'>
         <ColorPicker />
     </div>
